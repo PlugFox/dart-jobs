9c3c380c2c0c31e092a5736e1b1ffd915f2e049f
Plague Fox <plugfox@gmail.com>
2021-10-28 00:54:46 +0300

    [#38] вынес модели в shared каталог

---
+++ client/lib/src/feature/job/data/job_repository.dart
@@ -0,0 +1,156 @@
+import 'dart:math' as math show Random;
+
+import 'package:dart_jobs/src/feature/authentication/model/user_entity.dart';
+import 'package:dart_jobs/src/feature/job/data/job_network_data_provider.dart';
+import 'package:money2/money2.dart';
+
+import '../../../../../../shared/lib/src/models/proposal.dart';
+
+abstract class IJobRepository {
+  /// Создать новую работу
+  Future<Job> create({
+    required final AuthenticatedUser user,
+    required final String title,
+    required final String company,
+    required final String country,
+    required final String location,
+    required final bool remote,
+    required final Money salaryFrom,
+    required final Money salaryTo,
+    JobAttributes attributes = const JobAttributes.empty(),
+  });
+
+  /// Запросить данные работы по идентификатору
+  Future<Job> fetchById(final String id);
+
+  /// Запросить данные работы
+  Future<Job> fetch(final Job job);
+
+  /// Обновить работу
+  Future<void> update(final Job job);
+
+  /// Удалить работу по идентификатору
+  Future<void> deleteById(final String id);
+
+  /// Удалить работу
+  Future<void> delete(final Job job);
+}
+
+class JobRepositoryImpl implements IJobRepository {
+  final IJobNetworkDataProvider _networkDataProvider;
+  JobRepositoryImpl({
+    required final IJobNetworkDataProvider networkDataProvider,
+  }) : _networkDataProvider = networkDataProvider;
+
+  @override
+  Future<Job> create({
+    required final AuthenticatedUser user,
+    required final String title,
+    required final String company,
+    required final String country,
+    required final String location,
+    required final bool remote,
+    required final Money salaryFrom,
+    required final Money salaryTo,
+    JobAttributes attributes = const JobAttributes.empty(),
+  }) =>
+      _networkDataProvider.create(
+        user: user,
+        title: title,
+        company: company,
+        country: country,
+        location: location,
+        remote: remote,
+        salaryFrom: salaryFrom,
+        salaryTo: salaryTo,
+        attributes: attributes,
+      );
+
+  @override
+  Future<Job> fetchById(String id) => _networkDataProvider.fetchById(id);
+
+  @override
+  Future<Job> fetch(Job job) => fetchById(job.id);
+
+  @override
+  Future<void> update(Job job) => _networkDataProvider.update(job);
+
+  @override
+  Future<void> deleteById(String id) => _networkDataProvider.deleteById(id);
+
+  @override
+  Future<void> delete(Job job) => deleteById(job.id);
+}
+
+class JobRepositoryFake implements IJobRepository {
+  static math.Random get _rnd => __rnd ??= math.Random();
+  static math.Random? __rnd;
+  static final Map<String, Job> _jobs = <String, Job>{};
+
+  @override
+  Future<Job> create({
+    required final AuthenticatedUser user,
+    required final String title,
+    required final String company,
+    required final String country,
+    required final String location,
+    required final bool remote,
+    required final Money salaryFrom,
+    required final Money salaryTo,
+    JobAttributes attributes = const JobAttributes.empty(),
+  }) async {
+    final now = DateTime.now();
+    final newJob = Job(
+      id: DateTime.now().millisecondsSinceEpoch.toRadixString(36),
+      creatorId: user.uid,
+      title: title,
+      company: company,
+      country: country,
+      location: location,
+      remote: remote,
+      salaryFrom: salaryFrom,
+      salaryTo: salaryTo,
+      attributes: attributes,
+      created: now,
+      updated: now,
+    );
+    await Future<void>.delayed(const Duration(seconds: 1));
+    _jobs[newJob.id] = newJob;
+    return newJob;
+  }
+
+  @override
+  Future<Job> fetchById(final String id) async {
+    await Future<void>.delayed(const Duration(seconds: 1));
+    final now = DateTime.now();
+    return _jobs[id] ??= Job(
+      id: id,
+      creatorId: _rnd.nextInt(2).toRadixString(36),
+      title: 'Some job',
+      company: 'company',
+      country: 'country',
+      location: 'location',
+      remote: true,
+      created: now,
+      updated: now,
+    );
+  }
+
+  @override
+  Future<Job> fetch(final Job job) => fetchById(job.id);
+
+  @override
+  Future<void> update(final Job job) async {
+    await Future<void>.delayed(const Duration(seconds: 1));
+    _jobs[job.id] = job;
+  }
+
+  @override
+  Future<void> deleteById(final String id) async {
+    await Future<void>.delayed(const Duration(seconds: 1));
+    _jobs.remove(id);
+  }
+
+  @override
+  Future<void> delete(final Job job) => deleteById(job.id);
+}
---
+++ shared/lib/src/utils/date_util.dart
@@ -0,0 +1,15 @@
+import 'package:meta/meta.dart';
+
+/// Namespace
+@sealed
+abstract class DateUtil {
+  DateUtil._();
+
+  /// Дату в миллисекунды UnixTime
+  static int toUnixTime(final DateTime dateTime) =>
+      dateTime.millisecondsSinceEpoch;
+
+  /// Миллисекунды UnixTime в дату
+  static DateTime fromUnixTime(final int millisecondsSinceEpoch) =>
+      DateTime.fromMillisecondsSinceEpoch(millisecondsSinceEpoch);
+}
