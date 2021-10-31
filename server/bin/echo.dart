a42540958f293d802bc47f9e67b0637366a470f5
Plague Fox <plugfox@gmail.com>
2021-10-31 12:50:29 +0300

    [#38] добавил набросок конфигурации серверной части

---
+++ server/bin/echo.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+// Configure routes.
+final _router = Router()
+  ..get('/', _rootHandler)
+  ..get('/echo/<message>', _echoHandler);
+
+Response _rootHandler(Request req) {
+  return Response.ok('Hello, World!\n');
+}
+
+Response _echoHandler(Request request) {
+  final message = request.params['message'];
+  return Response.ok('$message\n');
+}
+
+void main(List<String> args) async {
+  print('Run echo server with args: $args');
+
+  // Use any available host or container IP (usually `0.0.0.0`).
+  final ip = InternetAddress.anyIPv4;
+
+  // Configure a pipeline that logs requests.
+  final _handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+
+  /// TODO: Ожидать сигнала завершения
+
+  // For running in containers, we respect the PORT environment variable.
+  final port = int.parse(Platform.environment['port'] ?? '80');
+  final server = await serve(_handler, ip, port);
+  print('Server listening on port ${server.port}');
+}
