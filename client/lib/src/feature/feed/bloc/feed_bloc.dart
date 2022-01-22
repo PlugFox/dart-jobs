54352237b4d6bdf7745e4bd32aefbd854677aea5
Plague Fox <plugfox@gmail.com>
2022-01-22 18:00:05 +0300

    [#68] feed simple combine algorithm

--- client/lib/src/feature/feed/bloc/feed_bloc.dart
+++ client/lib/src/feature/feed/bloc/feed_bloc.dart
@@ -107,10 +107,7 @@ class FeedState with _$FeedState {
   }) = _ErrorFeedState;
 }
 
-class FeedBLoC extends Bloc<FeedEvent, FeedState> {
-  DateTime _lastFetchRecent = DateTime.now();
-  Timer? _timer;
-  final IJobRepository _repository;
+class FeedBLoC extends Bloc<FeedEvent, FeedState> with _CombineMixin {
   FeedBLoC({
     required final IJobRepository repository,
     final FeedState initialState = const FeedState.idle(
@@ -120,15 +117,14 @@ class FeedBLoC extends Bloc<FeedEvent, FeedState> {
     ),
   })  : _repository = repository,
         super(initialState) {
-    /// TODO: подписка на уведомления с сервера о новых элементах
     _timer = Timer.periodic(
-      const Duration(minutes: 5),
+      const Duration(minutes: 15),
       (_) {
         final now = DateTime.now();
         if (!state.isProcessed &&
             _lastFetchRecent.isBefore(
               now.subtract(
-                const Duration(minutes: 3),
+                const Duration(minutes: 5),
               ),
             )) {
           add(const FeedEvent.fetchRecent());
@@ -145,207 +141,97 @@ class FeedBLoC extends Bloc<FeedEvent, FeedState> {
     );
   }
 
-  // ignore: long-method
+  DateTime _lastFetchRecent = DateTime.now();
+  Timer? _timer;
+  final IJobRepository _repository;
+
   Future<void> _fetchRecent(_FetchRecentFeedEvent event, Emitter<FeedState> emit) async {
     try {
       emit(event.inProgress(state: state));
-
       JobsChunk chunk;
       do {
         final updatedAfter = state.list.firstOrNull?.updated ?? DateTime.now().subtract(const Duration(days: 1));
-        final exclude = <int>[];
-        for (final job in state.list) {
-          if (!job.updated.isAtSameMomentAs(updatedAfter)) break;
-          exclude.add(job.id);
-        }
-        chunk = await _repository
-            .getRecent(
-              updatedAfter: updatedAfter,
-              filter: state.filter,
-              exclude: exclude,
-            )
-            .timeout(const Duration(seconds: 15));
-
-        if (chunk.isEmpty) {
-          break;
-        }
-
-        // Список обновленных (их нужно исключить из пред идущего состояния)
-        // тк они будут перемещены в начало ленты
-        final idsForExclusion = List<int>.of(
-          chunk
-              .where(
-                (e) =>
-                    e.deletionMark ||
-                    !e.updated.isAtSameMomentAs(e.created) ||
-                    e.updated.isAtSameMomentAs(updatedAfter),
+        {
+          final exclude = <int>[];
+          for (final job in state.list) {
+            if (!job.updated.isAtSameMomentAs(updatedAfter)) break;
+            exclude.add(job.id);
+          }
+          chunk = await _repository
+              .getRecent(
+                updatedAfter: updatedAfter,
+                filter: state.filter,
+                exclude: exclude,
               )
-              .map<int>((e) => e.id),
-        );
+              .timeout(const Duration(seconds: 15));
+          exclude.clear();
+          if (chunk.isEmpty) {
+            break;
+          }
+        }
         await Future<void>.delayed(Duration.zero);
-
-        // Исключаю из исходной коллекции обновленные и удаленные
-        // а затем объединяю исходный список и обновление
-        final list = await _reducedStateList(idsForExclusion).toList().then<List<Job>>(
-              (list) => <Job>[
-                ...chunk.where((e) => !e.deletionMark),
-                ...list,
-              ],
-            );
-
+        final list = await _insertAtTheBeginning(chunk);
         emit(event.inProgress(state: state, list: list));
-
         // Считаю что это конец списка и больше не запрашиваю,
         // если количество полученных меньше чем половина запрошенных
         // этот допуск нужен для того, чтоб исключить "ошибочные записи"
       } while (chunk.length > state.filter.limit ~/ 2);
       _lastFetchRecent = DateTime.now();
-
       emit(event.successful(state: state));
     } on GraphQLJobException {
       emit(event.error(state: state, message: 'Network exception'));
       rethrow;
     } on Object catch (err) {
       l.e('Произошла ошибка при запросе последних элементов ленты: $err');
-      emit(
-        event.error(
-          state: state,
-          message: 'An error occurred while updating the feed',
-        ),
-      );
+      emit(event.error(state: state, message: 'An error occurred while updating the feed'));
       rethrow;
     } finally {
       emit(event.idle(state: state));
     }
   }
 
-  // ignore: long-method
   Future<void> _paginate(_PaginateFeedEvent event, Emitter<FeedState> emit) async {
-    if (state.endOfList) {
-      return;
-    }
+    if (state.endOfList) return;
     try {
-      emit(
-        event.inProgress(
-          state: state,
-          endOfList: false,
-        ),
-      );
-
-      final updatedBefore = state.list.lastOrNull?.updated ?? DateTime.now().add(const Duration(days: 1));
-      final exclude = <int>[];
-      for (final job in state.list.reversed) {
-        if (!job.updated.isAtSameMomentAs(updatedBefore)) break;
-        exclude.add(job.id);
-      }
-      final chunk = await _repository
-          .paginate(
-            updatedBefore: updatedBefore,
-            filter: state.filter,
-            exclude: exclude,
-          )
-          .timeout(const Duration(seconds: 15));
-
-      // break if empty
-      if (chunk.isEmpty) {
-        emit(
-          event.successful(
-            state: state,
-            endOfList: true,
-          ),
-        );
-        return;
-      }
-
-      // Список более старых (если updatedBefore совпадает с updated исключаю из предидущего состояния эти id)
-      final idsForExclusion = List<int>.of(
-        chunk
-            .where(
-              (e) => e.updated.isAtSameMomentAs(updatedBefore),
+      emit(event.inProgress(state: state, endOfList: false));
+      JobsChunk chunk;
+      {
+        final updatedBefore = state.list.lastOrNull?.updated ?? DateTime.now().add(const Duration(days: 1));
+        final exclude = <int>[];
+        for (final job in state.list.reversed) {
+          if (!job.updated.isAtSameMomentAs(updatedBefore)) break;
+          exclude.add(job.id);
+        }
+        chunk = await _repository
+            .paginate(
+              updatedBefore: updatedBefore,
+              filter: state.filter,
+              exclude: exclude,
             )
-            .map<int>((e) => e.id),
-      );
+            .timeout(const Duration(seconds: 15));
+        exclude.clear();
+        if (chunk.isEmpty) {
+          emit(event.successful(state: state, endOfList: true));
+          return;
+        }
+      }
       await Future<void>.delayed(Duration.zero);
-
-      // Исключаю из исходной коллекции обновленные и удаленные
-      // а затем объединяю исходный список и обновление
-      final list = await _reducedStateList(idsForExclusion).toList().then<List<Job>>(
-            (list) => <Job>[
-              ...list,
-              ...chunk.where((e) => !e.deletionMark),
-            ],
-          );
-      //await Future<void>.delayed(const Duration(seconds: 10));
-      emit(
-        event.successful(
-          state: state,
-          list: list,
-          endOfList: chunk.endOfList,
-        ),
-      );
+      final list = await _insertAtTheEnd(chunk);
+      emit(event.successful(state: state, list: list, endOfList: chunk.endOfList));
     } on GraphQLJobException {
       emit(event.error(state: state, message: 'Network exception'));
       rethrow;
     } on Object catch (err) {
       l.e('Произошла ошибка при обновлении ленты: $err');
-      emit(
-        event.error(
-          state: state,
-          message: 'An error occurred while updating the feed',
-        ),
-      );
+      emit(event.error(state: state, message: 'An error occurred while updating the feed'));
       rethrow;
     } finally {
-      emit(
-        event.idle(state: state),
-      );
+      emit(event.idle(state: state));
     }
   }
 
   Future<void> _setFilter(_SetFilterFeedEvent event, Emitter<FeedState> emit) =>
       Future<void>(() => emit(event.setNewFilter(state: state)));
-
-  /// Исключаю из исходной коллекции элементы
-  Stream<Job> _reducedStateList(List<int> idsForExclusion) async* {
-    // Если исходный список пуст - нечего проверять
-    if (state.list.isEmpty) return;
-    // Если список на исключение пуст - возвращаем исходную коллекцию
-    if (idsForExclusion.isEmpty) {
-      yield* Stream<Job>.fromIterable(state.list);
-      return;
-    }
-    // Список идентификаторов на исключение
-    final ids = List<int>.of(idsForExclusion);
-    final sw = Stopwatch()..start();
-    for (final job in state.list) {
-      // Обход обратной выборкой, right fold
-      var idx = ids.length - 1;
-      if (idx < -1) {
-        // Если список на исключение пуст - возвращаем как есть
-        yield job;
-      } else {
-        // Сравниваем идентификатор текущего элемента с идентификаторами на исключение
-        while (idx >= 0) {
-          // Ели идентификаторы совпали - исключаем идентификатор на исключение
-          if (job.id == ids[idx]) {
-            ids.removeAt(idx);
-            break;
-          }
-          idx--;
-        }
-        // Если не один из идентификаторов не подошел - возвращаем как есть
-        if (idx < 0) {
-          yield job;
-        }
-      }
-      // Если прошло больше 8 мс - уступаем другим событиям в эвент лупе
-      if (sw.elapsedMilliseconds > 8) {
-        await Future<void>.delayed(Duration.zero);
-        sw.reset();
-      }
-    }
-    sw.stop();
-  }
 
   @override
   Future<void> close() {
@@ -468,4 +354,53 @@ mixin _IdleStateEmitter on FeedEvent {
         endOfList: state.endOfList,
         message: message ?? 'Idle',
       );
+}
+
+/* Миксины для БЛоК'а */
+
+mixin _CombineMixin on BlocBase<FeedState> {
+  Future<List<Job>> _insertAtTheBeginning(JobsChunk chunk) async {
+    if (chunk.isEmpty) {
+      return state.list;
+    }
+    // Список обновленных и удаленных (их нужно исключить из пред идущего состояния)
+    // тк они будут или перемещены в начало ленты или исключены из выборки
+    final idsForExclusion = chunk
+        .where(
+          (e) => !e.updated.isAtSameMomentAs(e.created) || e.deletionMark,
+        )
+        .map<int>((e) => e.id)
+        .toList();
+    await Future<void>.delayed(Duration.zero);
+    // Формирую новый список из новых данных и текущего состояния
+    final list = <Job>[
+      ...chunk.where((e) => !e.deletionMark),
+      ...state.list.whereNot((job) {
+        for (var idx = 0; idx < idsForExclusion.length; idx++) {
+          if (job.id == idsForExclusion[idx]) {
+            idsForExclusion.removeAt(idx);
+            return true;
+          }
+        }
+        return false;
+      }),
+    ];
+    await Future<void>.delayed(Duration.zero);
+    list.sort();
+    return list;
+  }
+
+  Future<List<Job>> _insertAtTheEnd(JobsChunk chunk) async {
+    if (chunk.isEmpty) {
+      return state.list;
+    }
+    // Формирую новый список из новых данных и текущего состояния
+    final list = <Job>[
+      ...state.list,
+      ...chunk.where((e) => !e.deletionMark),
+    ];
+    await Future<void>.delayed(Duration.zero);
+    list.sort();
+    return list;
+  }
 }
