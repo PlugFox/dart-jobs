a0230f843458a6c40eb3057db0cbbe0a3ac8d8f4
Plague Fox <plugfox@gmail.com>
2022-02-08 16:34:02 +0300

    [quickfix] change theme in web

--- client/lib/src/feature/settings/widget/platform/platform_web.dart
+++ client/lib/src/feature/settings/widget/platform/platform_web.dart
@@ -4,10 +4,11 @@ import 'package:flutter/material.dart';
 
 void changeTheme(String theme) {
   ThemeData.dark();
-  final color = theme == 'dark' ? '#212121' : '#2196F3';
-  _setMeta('theme-color', color);
-  _setMeta('msapplication-navbutton-color', color);
-  html.document.body?.style.backgroundColor = color;
+  final primaryColor = theme == 'dark' ? '#212121' : '#2196f3';
+  final backgroundColor = theme == 'dark' ? '#303030' : '#fafafa';
+  _setMeta('theme-color', primaryColor);
+  _setMeta('msapplication-navbutton-color', primaryColor);
+  html.document.body?.style.backgroundColor = backgroundColor;
 }
 
 void _setMeta(String key, String value) {
